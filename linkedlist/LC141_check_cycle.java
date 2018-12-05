8a6cfd313ea38111d1f88a1c88ee62832bbed6e8
Bast Tan <bast.p.tan@gmail.com>
2018-12-05 02:18:51 -0800

    check cycle in linkedlist, slow fast

---
+++ linkedlist/LC141_check_cycle.java
@@ -0,0 +1,37 @@
+
+// s1, if node has been visited, or in the set, true
+public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) return false;
+    Set<ListNode> visited = new HashSet<>();
+
+    for (ListNode curr = head; curr != null; curr = curr.next) {
+        if (visited.contains(curr)) return true;
+        visited.add(curr); // add before change curr
+    }
+    return false;
+}
+// s2. if has cycle, slow, fast will meet in the cycle
+// if slow = fast = head, walk first then check
+// if slow + 1 = fast, check first then walk
+public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) return false;
+    ListNode slow = head, fast = head;
+    
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
+public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) return false;
+    ListNode slow = head, fast = head.next;
+    
+    while (fast != null && fast.next != null) {
+        if (slow == fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return false;
+}
