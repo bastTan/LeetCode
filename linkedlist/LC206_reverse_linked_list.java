520a8603dd2c2dd2661dcec57995a967d2a56c5a
Bast Tan <bast.p.tan@gmail.com>
2018-12-05 13:42:48 -0800

    reverse list with cycle

--- linkedlist/LC206_reverse_linked_list.java
+++ linkedlist/LC206_reverse_linked_list.java
@@ -29,3 +29,37 @@ public ListNode reverseListRec(ListNode head) {
     curr.next = null;
     return newHead;
 }
+
+// reverse linkedlist might has cycle
+// use node.visited field
+public ListNode reverseListIter(ListNode head) {
+    if (head == null || head.next == null) return head;
+
+    // 1 -> 3 -> 5
+    // 1 <- 3 <- 5
+    ListNode curr = head;
+    ListNode pre = null, next  = null;
+    while (!curr.visited) {
+        curr.visited = true;
+        next = curr.next; 
+        curr.next = pre;
+        pre = curr;
+        curr = next;
+    }
+}
+// use hashSet
+public ListNode reverseListIter(ListNode head) {
+    if (head == null || head.next == null) return head;
+    Set<ListNode> visited = new HashSet<>();
+    // 1 -> 3 -> 5
+    // 1 <- 3 <- 5
+    ListNode curr = head;
+    ListNode pre = null, next  = null;
+    while (!visited.contains(curr)) {
+        visited.add(curr);
+        next = curr.next; 
+        curr.next = pre;
+        pre = curr;
+        curr = next;
+    }
+}
